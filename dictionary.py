a3b2f89a2d7cc4903ca5998c7c170be4e2ca8d4c
arpit0633 <63409224+arpit0633@users.noreply.github.com>
2021-07-10 18:08:10 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+d = {
+    "name":"arpit",
+    "age":19,
+    "ar":"pit",
+    "hobby":["cricket","movie","anime","coding"],
+    "fav":{"a":{"c","f"}},
+    "exc":{"bisep","chest","shoulder"}
+    }
+# t={"rishi":"gym","yagna":"look","hobby":"gym"}
+# d.update(t)
+# print(d)
+# d.popitem()
+# print(d)
+# print("coding" in d.values())
+# u=d.keys()
+# print(u)
+# print(type(u))
+# print(d.items())
+# to check dict in dict write whole dict
